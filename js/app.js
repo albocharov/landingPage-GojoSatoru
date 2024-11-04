b2f8d9d18ed261b67f8ea303c322ed616a164570
Alexander Bocharov <fireon4ik229@mail.ru>
2024-11-04 12:52:49 +0700

    dev[]: сверстан весь урок (все блоки). Кривая верстка без адаптива под разные высоты экрана

---
+++ js/app.js
@@ -0,0 +1,61 @@
+/*========= menu icon navbar =========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+};
+
+/*========= scroll sections active link =========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+	sections.forEach(section => {
+		let top = window.scrollY;
+		let offset = section.offsetTop - 150;
+		let height = section.offsetHeight;
+		let id = section.getAttribute('id');
+
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach(links => {
+				links.classList.remove('active');
+				document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+			});
+		}
+	});
+	
+	/*========= sticky navbar =========*/
+	let header = document.querySelector('.header');
+	
+	header.classList.toggle('sticky', window.scrollY > 100);
+	
+	/*========= remove menu icon navbar when scrolling =========*/
+	menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
+};
+
+/*========= swiper =========*/
+const swiper = new Swiper('.mySwiper', {
+	slidesPerView: 1,
+	spaceBetween: 50,
+	loop: true,
+	grabCursor: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+});
+
+/*========= darkMode =========*/
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+	document.body.classList.toggle('dark');
+	darkModeIcon.classList.toggle('bx-sun');
+}
