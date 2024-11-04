f29577a7b96e5e0ceccc1c5b4f24de1e9652dc53
Alexander Bocharov <fireon4ik229@mail.ru>
2024-11-04 14:39:40 +0700

    dev[]: фикс адаптива. Добавлен контент. Добавлен scroll reveal

--- js/app.js
+++ js/app.js
@@ -59,3 +59,15 @@ darkModeIcon.onclick = () => {
 	document.body.classList.toggle('dark');
 	darkModeIcon.classList.toggle('bx-sun');
 }
+
+/*========= scroll reveal =========*/
+ScrollReveal({
+	distance: '80px',
+	duration: 2000,
+	delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image img, .students-box, .testimonial-wrapper', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
